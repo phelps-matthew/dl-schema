f22cdc10baacc8f5cea97756a0e5837125f57be1
Matthew Phelps <mgp333@gmail.com>
2022-01-27 19:45:00 -1000

    add setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+setuptools.setup(
+    name="dl_schema",
+    description="Template for deep learning projects.",
+    version="1.0.0",
+    author="Matthew Phelps",
+    author_email="matthewphelps@odysseyconsult.com",
+    packages=setuptools.find_packages(),
+)
