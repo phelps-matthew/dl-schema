d0ca1488ad8131eba3bce38d0e16ec60b3c154f6
Matthew Phelps <mgp333@gmail.com>
2022-01-28 17:21:09 -1000

    clean up non essential utils

--- dl_schema/utils.py
+++ dl_schema/utils.py
@@ -9,6 +9,7 @@ import random
 import torch
 import collections
 
+
 def set_seed(seed):
     random.seed(seed)
     np.random.seed(seed)
@@ -16,38 +17,8 @@ def set_seed(seed):
     torch.cuda.manual_seed_all(seed)
 
 
-def quaternion_to_so3(q: list) -> np.ndarray:
-    """Convert quaternion to SO(3) representation according to `speedplus-utils`
-    convention. Compared to Wikipedia definition, this quantity is transposed
-    since q is give in the annotation as q_C, i.e. the camera reference frame.
-
-    Args:
-        q: list of shape (4); quaternion (possibly unnormalized)
-
-    Returns:
-        so3: np.ndarray of shape (3,3)
-    """
-    q = q / np.linalg.norm(q)  # normalize
-    q0, q1, q2, q3 = q[0], q[1], q[2], q[3]
-    so3 = np.zeros((3, 3))
-
-    so3[0, 0] = 2 * q0 ** 2 - 1 + 2 * q1 ** 2
-    so3[1, 1] = 2 * q0 ** 2 - 1 + 2 * q2 ** 2
-    so3[2, 2] = 2 * q0 ** 2 - 1 + 2 * q3 ** 2
-
-    so3[0, 1] = 2 * q1 * q2 + 2 * q0 * q3
-    so3[0, 2] = 2 * q1 * q3 - 2 * q0 * q2
-
-    so3[1, 0] = 2 * q1 * q2 - 2 * q0 * q3
-    so3[1, 2] = 2 * q2 * q3 + 2 * q0 * q1
-
-    so3[2, 0] = 2 * q1 * q3 + 2 * q0 * q2
-    so3[2, 1] = 2 * q2 * q3 - 2 * q0 * q1
-
-    return so3
-
-
-def flatten(d, parent_key='', sep='.'):
+def flatten(d, parent_key="", sep="."):
+    """Flatten a nested dictionary"""
     items = []
     for k, v in d.items():
         new_key = parent_key + sep + k if parent_key else k
@@ -57,14 +28,15 @@ def flatten(d, parent_key='', sep='.'):
             items.append((new_key, v))
     return dict(items)
 
+
 def load_yaml(path: Path):
-    """Load yaml as dict.
+    """deserialize yaml as dict
 
     Args:
         path: Path to .yaml, .yml, or .json file.
     """
     if Path(path).suffix == ".json":
-        return load_from_json(path)
+        return load_json(path)
 
     with open(path, "r") as stream:
         try:
@@ -74,39 +46,15 @@ def load_yaml(path: Path):
 
 
 def save_yaml(path, data_dict):
-    """Export dict as yaml."""
+    """serialize dict to yaml."""
     # Check if parent directories of path exist; if not, make them.
     Path(path).parent.absolute().mkdir(parents=True, exist_ok=True)
-
-    # Store data (serialize).
     with open(path, "w") as handle:
         yaml.dump(data_dict, handle, default_flow_style=None, sort_keys=False)
 
 
-def make_clean_dir(directory):
-    if os.path.exists(directory):
-        shutil.rmtree(directory)
-    os.makedirs(directory)
-
-
-def search_for_files(curr_path: str, file_pattern: str, resolve=False) -> list:
-    """Find all paths matching pattern while recurring through directory."""
-    file_list = []
-    for p in Path(curr_path).rglob(file_pattern):
-        if resolve:
-            p = p.resolve()
-        file_list.append(str(p))
-    return file_list
-
-
-def load_from_json(json_file):
-    """
-    Pull a detection dictionary into memory from a JSON file.
-
-    :param json_file: path to the JSON file
-    :return: dictionary of detections
-    """
-
+def load_json(json_file):
+    """deserialize json as dict"""
     with open(json_file, "rb") as handle:
         unserialized_data = json.load(handle)
         handle.close()
