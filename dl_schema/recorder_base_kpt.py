4fc6fd5dbb0a2d507ec44481e8686a7557d3897a
mphelps <matthewphelps@odysseyconsult.com>
2022-05-24 15:53:33 -1000

    add recoder

--- dl_schema/recorder_base_kpt.py
+++ dl_schema/recorder_base_kpt.py
@@ -1,68 +1,42 @@
 """
-Logging base class for training runs. The philosophy is that each repo will require its own unique
-analytics and thus is to inherit the RecorderBase and implelemnt custom logic. The base class here 
-provides all the common utilities and methods shared among different ML projects.
+Logging utility for training runs
+
+See https://github.com/microsoft/qlib/blob/d7d19feb4ebb0c4318ac3bfda32a34c56e28a6a0/qlib/utils/paral.py
 """
-from functools import partial
-import logging
 import math
-from pathlib import Path
-from queue import Queue
+import logging
+from functools import partial
 from threading import Thread
 from typing import Callable
-
-import PIL
-from matplotlib import pyplot as plt
-import mlflow
+from queue import Queue
+from pathlib import Path
 import numpy as np
+import mlflow
 import torch
-import torchvision
 
 logger = logging.getLogger(__name__)
 
 
 class AsyncCaller:
     """
-    Implements asynchronous execution of class methods within an arbitrary class by
-    providing a convenient decorator `async_dec`. Uses a single queue and multiplethreads
-    (workers) to consume the queue (i.e. consumer/producer pattern).
-
-    Usage:
-        Decorate a class method via `@AsyncCaller.async_dec(ac_attr="async_log")`. The
-        target class must possess the corresponding attribute `async_log = AsyncCaller()`.
-
-    Note:
-        In applying the decorator @AsyncCaller.async_dec(ac_attr="async_log"), the
-        async_dec method will look at the class instance (e.g. Recoder or RecorderBase)
-        and determine if there is an attribute named 'async_log' and if that attribute is
-        callable. If true, serves as the indicator that async calling is to be applied.
-
-        Additionally, the `wrapper` function is only called when the decorated method is
-        called, otherwise the decorated methods simply point to the `wrapper` function.
+    This AsyncCaller tries to make it easier to async call
+    Currently, it is used in MLflowRecorder to make functions like `log_params` async
+    NOTE:
+    - This caller didn't consider the return value
     """
 
     STOP_MARK = "__STOP"
 
-    def __init__(self, num_threads=4) -> None:
+    def __init__(self) -> None:
         self._q = Queue()
         self._stop = False
-        self.num_threads = num_threads
-        self.threads = []
-        self.start_threads()
-
-    def start_threads(self):
-        """spin up n threads actively consuming queue via `run`"""
-        for _ in range(self.num_threads):
-            t = Thread(target=self.run)
-            self.threads.append(t)
-            t.start()
+        self._t = Thread(target=self.run)
+        self._t.start()
 
     def close(self):
-        """add STOP_MARK to queue to trigger thread termination"""
         self._q.put(self.STOP_MARK)
 
     def run(self):
-        """consume queue until STOP_MARK is reached"""
         while True:
             data = self._q.get()
             if data == self.STOP_MARK:
@@ -73,13 +47,9 @@ class AsyncCaller:
         self._q.put(partial(func, *args, **kwargs))
 
     def wait(self, close=True):
-        """block parent thread until async threads terminate"""
         if close:
-            for t in self.threads:
-                self.close()
-        for i, t in enumerate(self.threads):
-            logger.info(f"closing thread {i + 1}/{self.num_threads}")
-            t.join()
+            self.close()
+        self._t.join()
 
     @staticmethod
     def async_dec(ac_attr):
@@ -96,11 +66,7 @@ class AsyncCaller:
 
 
 class RecorderBase:
-    """
-    To be inherited by custom user-defined Recoder class. Provides core backend methods for
-    managing runs/experiments and assists in fine control of logging training run quantities.
-    Includes a number of image logging helper utilities to be called within child class.
-    """
+    """assists in fine control of logging training run quantities"""
 
     def __init__(self, cfg):
         self.cfg = cfg
@@ -159,19 +125,14 @@ class RecorderBase:
         logger.info(f"starting mlflow run: {Path(self.root).parent}")
 
         # initialize async logging
-        if self.cfg.log.enable_async:
-            n_threads = self.cfg.log.num_threads
-            logger.info(f"enabling async logging with {n_threads} threads")
-            # note matching "async_log" in AsyncCaller method decorations
-            self.async_log = AsyncCaller(num_threads=n_threads)
-        else:
-            self.async_log = None
+        logger.info("initializing async logging")
+        self.async_log = AsyncCaller() if self.cfg.log.enable_async else None
         return self.run
 
     def end_run(self):
+        """end mlflow run"""
         mlflow.end_run()
         if self.async_log is not None:
-            logger.info("waiting for recorder threads to finish")
             self.async_log.wait()
         self.async_log = None
 
@@ -203,46 +164,8 @@ class RecorderBase:
         self.client.log_text(self.run_id, text, artifact_path)
 
     @AsyncCaller.async_dec(ac_attr="async_log")
-    def log_image_grid(
-        self,
-        x: torch.Tensor,
-        name: str = "x",
-        NCHW: bool = True,
-        normalize: bool = True,
-        jpg: bool = True,
-        padding: bool = 1,
-    ):
-        """log batch of images
-
-        Args:
-            x: torch.float32 of shape (N, C, H, W) or (N, H, W, C)
-            name: filename of rendered image, not including file extension
-            NCHW: if false, will convert from NHWC to NCHW
-            normalize: apply per-instance normalization
-            jpg: if false, convert to png
-            padding: pixel padding between images in grid
-        """
-        N = x.shape[0]
-        n_rows = math.ceil(math.sqrt(N))  # actually n_cols
-
-        if x is not None:
-            if not NCHW:
-                x = x.permute(0, 3, 1, 2)
-            img_fmt = "jpg" if jpg else "png"
-            grid_x = torchvision.utils.make_grid(
-                x, normalize=normalize, nrow=n_rows, pad_value=1.0, padding=padding
-            ).permute(1, 2, 0)
-            self.client.log_image(self.run_id, grid_x.numpy(), f"{name}.{img_fmt}")
-
-    def figure_to_array(self, figure: plt.Figure) -> np.ndarray:
-        """convert matplotlib figure to numpy array"""
-        buffer = io.BytesIO()
-        plt.savefig(buffer, format="png", dpi=100)
-        plt.close(figure)
-        buffer.seek(0)
-        img = PIL.Image.open(buffer)
-        data = np.array(img)
-        return data
+    def log_iter(self, inputs, step=None, split="train"):
+        raise NotImplementedError
 
     def norm_batch(self, tensor: torch.Tensor):
         """normalize batch of images"""
@@ -250,8 +173,8 @@ class RecorderBase:
             self.norm_img(t)
         return tensor
 
-    def norm_img(self, img: torch.Tensor):
-        """normalize image to [0.0, 1.0] by clamping in range [low, high]"""
+    def norm_img(self, img):
+        """normalize image to [0.0, 1.0] by claming in range [low, high]"""
         low = float(img.min())
         high = float(img.max())
         img.clamp_(min=low, max=high)
