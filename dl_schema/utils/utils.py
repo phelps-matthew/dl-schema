519a45f6b8fbac85ec926ad325e2977f34acd44c
mphelps <matthewphelps@odysseyconsult.com>
2023-03-27 15:30:14 -1000

    update collection mutablemap base class reference for python>=3.10" "

--- dl_schema/utils/utils.py
+++ dl_schema/utils/utils.py
@@ -1,5 +1,5 @@
 """Utilities used across training repo"""
-import collections
+from collections.abc import MutableMapping
 import json
 import math
 from pathlib import Path
@@ -40,7 +40,7 @@ def flatten(d, parent_key="", sep="."):
     items = []
     for k, v in d.items():
         new_key = parent_key + sep + k if parent_key else k
-        if isinstance(v, collections.MutableMapping):
+        if isinstance(v, MutableMapping):
             items.extend(flatten(v, new_key, sep=sep).items())
         else:
             items.append((new_key, v))
